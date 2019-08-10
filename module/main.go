c5f096194706e06efef570e11ac45e620777e4a8
fannyhasbi <fnyhsbi@gmail.com>
2019-08-10 11:54:31 +0700

    Registering add function

--- module/main.go
+++ module/main.go
@@ -2,8 +2,25 @@ package main
 
 import (
 	"fmt"
+	"syscall/js"
 )
 
+func add(this js.Value, i []js.Value) interface{} {
+	result := js.ValueOf(i[0].Int() + i[1].Int())
+	fmt.Println("RESULT", result)
+
+	return result
+}
+
+func registerCallbacks() {
+	js.Global().Set("add", js.FuncOf(add))
+}
+
 func main() {
-	fmt.Println("Hello world")
+	c := make(chan struct{}, 0)
+
+	fmt.Println("WASM initialized")
+
+	registerCallbacks()
+	<-c
 }
